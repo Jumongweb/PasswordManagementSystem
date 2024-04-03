4ab997f1760341704b4cdc6dcf2d1d664c1e75de
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-04-03 16:24:27 +0100

    PasswordManagementException

---
+++ src/main/java/com/africa/semicolon/exceptions/PasswordManagementException.java
@@ -0,0 +1,7 @@
+package com.africa.semicolon.exceptions;
+
+public class PasswordManagementException extends RuntimeException{
+    public PasswordManagementException(String message){
+        super(message);
+    }
+}
