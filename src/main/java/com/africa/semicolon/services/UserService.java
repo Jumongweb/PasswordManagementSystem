b66c32ed4624fbf679a303a2c4a5fde90e624277
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-04-03 16:26:42 +0100

    UserService

--- src/main/java/com/africa/semicolon/services/UserService.java
+++ src/main/java/com/africa/semicolon/services/UserService.java
@@ -1,9 +1,27 @@
 package com.africa.semicolon.services;
 
+import com.africa.semicolon.data.models.User;
+import com.africa.semicolon.dtos.Request.LoginRequest;
+import com.africa.semicolon.dtos.Request.LogoutRequest;
 import com.africa.semicolon.dtos.Request.RegisterRequest;
+
+import java.util.List;
 
 public interface UserService {
 
     void register(RegisterRequest registerRequest);
 
+    long count();
+
+    User findUserBy(String username);
+
+    List<User> findAllUsers();
+
+    void deleteUserBy(String username);
+
+    void login(LoginRequest loginRequest);
+
+
+
+    void logout(LogoutRequest logoutRequest);
 }
